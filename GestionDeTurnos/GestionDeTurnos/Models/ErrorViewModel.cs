dc4ec050578f33fc72f11ca96c3bd0c5c7a8d47d
christopherdejesus <christophedejesus12@gmail.com>
2018-04-03 16:44:34 -0400

    commit1

---
+++ GestionDeTurnos/GestionDeTurnos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestionDeTurnos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
